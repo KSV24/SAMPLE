772800d3d70c5ab9b78f7c8c39aae52ec284c2a0
KSV24 <ksvish24@gmail.com>
2026-03-02 17:16:18 +0530

    lst

--- numpy_1.py
+++ numpy_1.py
@@ -1,5 +1,10 @@
-import numpy_1 as np
+#import numpy_1 as np
 
-arr = np.array([1,2,3,4,5])
-print(arr)
-print(type(arr))
+#arr = np.array([1,2,3,4,5])
+#print(arr)
+#print(type(arr))
+
+string=" virat kohli "
+print(string.strip())
+for character in string:
+    print(character,end=",")
