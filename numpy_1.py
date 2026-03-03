172fefc77ee1c0ba3d611120f89653471ea00de0
KSV24 <ksvish24@gmail.com>
2026-03-03 22:43:46 +0530

    edit

--- numpy_1.py
+++ numpy_1.py
@@ -4,7 +4,15 @@
 #print(arr)
 #print(type(arr))
 
-string=" virat kohli "
-print(string.strip())
-for character in string:
-    print(character,end=",")
+#string=" virat kohli "
+#print(string.strip())
+#for character in string:
+#    print(character,end=",")
+
+
+import numpy as np
+data = np.array([[10, 20, 30],
+                 [40, 50, 60]])
+print("Array:\n", data)
+print("Mean (row-wise):", np.mean(data, axis=1))
+print("Mean (column-wise):", np.mean(data, axis=0))
